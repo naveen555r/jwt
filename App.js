47bb76becda15784e411d9b58b9ff5b5120e6c1e
naveen555r <161932972+naveen555r@users.noreply.github.com>
2024-09-24 12:40:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './login page';
+import ProtectedPage from './protectedpage';
+
+
+function App() {
+  return (
+    <>
+     <LoginPage/>
+   <ProtectedPage/>
+  </>
+  )
+}
+
+export default App;
